e5456179781e6745934ef44391fa61cceb0f2ded
Mads Tinko Madsen <Lyderslev1@gmail.com>
2023-08-12 23:48:40 +0200

    Still trying to setup WorkSchedulePage.

--- lib/pages/WorkSchedulePage.dart
+++ lib/pages/WorkSchedulePage.dart
@@ -1,20 +1,115 @@
 import 'package:flutter/material.dart';
 
+class Task {
+  final String title;
+  final String time;
+  final String street;
+  final String zipcode;
+  final String city;
+
+  Task({
+    required this.title,
+    required this.time,
+    required this.street,
+    required this.zipcode,
+    required this.city,
+  });
+}
+
+class DayColumn extends StatelessWidget {
+  final String dayName;
+  final List<Task> tasks;
+
+  DayColumn({required this.dayName, required this.tasks});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.25,
+      margin: EdgeInsets.symmetric(horizontal: 8),
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 3,
+            blurRadius: 5,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            dayName,
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 16),
+          Column(
+            children: [
+              for (Task task in tasks)
+                Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.symmetric(vertical: 12, horizontal: 8),
+                  margin: EdgeInsets.only(bottom: 12),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        task.title,
+                        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 8),
+                      Text(task.time),
+                      SizedBox(height: 8),
+                      Text(task.street),
+                      Text('${task.zipcode} ${task.city}'),
+                    ],
+                  ),
+                ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 class WorkSchedulePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Work Schedule Page'),
-      ),
-      body: SingleChildScrollView(
+    return Container(
+      padding: EdgeInsets.all(8),
+      height: MediaQuery.of(context).size.height * 0.95,
+      child: SingleChildScrollView(
         scrollDirection: Axis.horizontal,
         child: Row(
           children: [
-            for (int i = 1; i <= 7; i++)
-              Padding(
-                padding: EdgeInsets.all(8.0), // Add padding between swimlanes
-                child: _buildSwimlaneCard(context, 'Swimlane $i'),
+            for (int i = 0; i < 7; i++)
+              ListView(
+                shrinkWrap: true,
+                children: [
+                  DayColumn(
+                    dayName: getDayName(i),
+                    tasks: List.generate(
+                      15,
+                      (index) => Task(
+                        title: 'Task ${index + 1}',
+                        time: '08:00 - 08:30',
+                        street: '123 Street',
+                        zipcode: '12345',
+                        city: 'City A',
+                      ),
+                    ),
+                  ),
+                ],
               ),
           ],
         ),
@@ -22,44 +117,20 @@ class WorkSchedulePage extends StatelessWidget {
     );
   }
 
-  Widget _buildSwimlaneCard(BuildContext context, String title) {
-    return Container(
-      width: MediaQuery.of(context).size.width * 0.6, // Adjust the width as needed
-      constraints: BoxConstraints(maxHeight: 200.0), // Limit vertical height
-      padding: EdgeInsets.all(16.0),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.3),
-            spreadRadius: 3,
-            blurRadius: 5,
-            offset: Offset(0, 3), // Shadow offset
-          ),
-        ],
-        border: Border.all(
-          color: Colors.black.withOpacity(0.1),
-          width: 1.0,
-        ),
-        borderRadius: BorderRadius.circular(8.0),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            title,
-            style: TextStyle(
-              fontSize: 18.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          SizedBox(height: 8.0),
-          Text('Column 1'),
-          Text('Column 2'),
-          Text('Column 3'),
-          // Add more columns as needed
-        ],
-      ),
-    );
+  String getDayName(int index) {
+    final now = DateTime.now();
+    final dayIndex = (now.weekday + index) % 7;
+    return ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'][dayIndex];
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Work Schedule'),
+      ),
+      body: WorkSchedulePage(),
+    ),
+  ));
 }
